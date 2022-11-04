35bff896a4064437307ff8fb95f5152fed72297d
Bernard Mordan <bernardmordan@gmail.com>
2022-11-04 13:08:42 +0000

    pushing blocks

--- src/index.js
+++ src/index.js
@@ -5,24 +5,40 @@ function preload() {
     this.load.atlasXML('bod', 'sprites.png', 'sprites.xml')
     this.load.image('spritesheet', 'sprites.png')
     this.load.tilemapTiledJSON('map', 'map.json')
+    this.load.image('crate', 'Crate_Brown.png')
 }
 
 function create() {
-    this.bod = this.physics.add.sprite(100, 100, 'bod')
+    const map = this.make.tilemap({key: 'map', tileWidth: 64, tileHeight: 64})
+    
+    this.cursors = this.input.keyboard.createCursorKeys()
+
+    const spawnpoint = map.findObject('spawn', obj => obj.name === 'spawn')
+
+    this.bod = this.physics.add.sprite(spawnpoint.x, spawnpoint.y, 'bod')
     this.bod.setCollideWorldBounds(true)
     this.bod.setDepth(1)
     const bodAnimations = getBodAnimations(this)
-    bodAnimations.forEach(animation => this.anims.create(animation))
-    // staticTileSet
-    this.cursors = this.input.keyboard.createCursorKeys()
-    
-    const map = this.make.tilemap({key: 'map', tileWidth: 64, tileHeight: 64})
-    const tileset = map.addTilesetImage('tileset', 'spritesheet')
-    const groundLayer = map.createLayer('ground', tileset, 0, 0)
-    const wallLayer = map.createLayer('walls', tileset, 0, 0)
+    bodAnimations.forEach(animation => this.anims.create(animation))    
 
+    const tileset = map.addTilesetImage('tileset', 'spritesheet')
+    map.createLayer('ground', tileset, 0, 0)
+    
+    const wallLayer = map.createLayer('walls', tileset, 0, 0)
     this.physics.add.collider(this.bod, wallLayer)
-    wallLayer.setCollisionBetween(2,2)
+    wallLayer.setCollisionBetween(25,25)
+
+    const crates = map.createFromObjects('crates', {gid: 29})
+    crates.forEach(crate => {
+        const crateSprite = this.physics.add.sprite(crate.x, crate.y, 'crate')
+        this.physics.add.collider(this.bod, crateSprite)
+        this.physics.add.collider(crateSprite, wallLayer)
+        crateSprite
+            .setPushable(true)
+            .setCollideWorldBounds(true)
+            .setDrag(2000, 2000)
+    })
+    
 }
 
 function update() {
@@ -50,8 +66,8 @@ function update() {
 
 new Phaser.Game({
     type: Phaser.AUTO,
-    width: 16 * 64,
-    height: 16 * 64,
+    width: 8 * 64,
+    height: 8 * 64,
     backgroundColor: '#FFF',
     physics: {
         default: 'arcade',
